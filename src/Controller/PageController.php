80dd37b170e55594053afe2f7ad42514b043c777
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-09-26 11:14:24 +0200

    first commit

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,15 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Cliente;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    /**
-     * @Route("/page", name="page")
-     */
+    #[Route('/page', name: 'app_page')]
     public function index(): Response
     {
         return $this->render('page/index.html.twig', [
@@ -18,11 +18,58 @@ class PageController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/", name="inicio")
-     */
-    public function inicio(): Response
+    #[Route('/cliente/insertar/{nombre}/{telefono}/{coche}', name: 'insertar_cliente')]
+    public function insertar(string $nombre, string $telefono,string $coche, ManagerRegistry $doctrine ): Response
     {
-        return $this->render('inicio.html.twig');
+      $entityManager = $doctrine->getManager();
+      $cliente = new Cliente();
+      $cliente->setNombre($nombre);
+      $cliente->setTelefono($telefono);
+      $cliente->setCoche($coche);
+      $entityManager->persist($cliente);
+        try {
+            $entityManager->flush();
+            return new Response("Cliente guardado");
+        }catch ( \Exception $e ) {
+            return new Response("No creado");
+        }
     }
+    #[Route('/cliente/buscar/{id}', name: 'buscar_cliente')]
+    public function buscar(int $id, ManagerRegistry $doctrine ): Response
+    {
+        $repository = $doctrine->getRepository(Cliente::class);
+        $cliente = $repository->find($id);
+
+        /*
+         * dump($clientes);
+        exit();
+         * */
+        return $this->render('ficha_cliente.html.twig', ['cliente' => $cliente]);
+
+    }
+    #[Route('/cliente/listar', name: 'listar_clientes')]
+    public function listarClientes(ManagerRegistry $doctrine ): Response
+    {
+        $repository = $doctrine->getRepository(Cliente::class);
+        $clientes = $repository->findAll();
+
+        /*
+         * dump($clientes);
+        exit();
+         * */
+        return $this->render('lista_clientes.html.twig', ['clientes' => $clientes]);
+    }
+
+    #[Route('/cliente/update/{id}/{nombre}', name: 'actualizar_cliente')]
+    public function actualizar(int $id, string $nombre, string $telefono, string $coche ,Cliente $cliente, ManagerRegistry $doctrine ): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $cliente->setNombre($nombre);
+        $cliente->setTelefono($telefono);
+        $cliente->setCoche($coche);
+        $entityManager->persist($cliente);
+        return $this->render('lista_clientes.html.twig', ['cliente' => $cliente]);
+    }
+
+
 }
---
+++ src/Entity/Cliente.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClienteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClienteRepository::class)]
+class Cliente
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 15)]
+    private ?string $telefono = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $coche = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?string
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(string $telefono): self
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    public function getCoche(): ?string
+    {
+        return $this->coche;
+    }
+
+    public function setCoche(string $coche): self
+    {
+        $this->coche = $coche;
+
+        return $this;
+    }
+}
